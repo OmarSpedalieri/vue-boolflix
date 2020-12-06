166cbeccad259df88d91e9a326e8c61f7033b082
Omar <omarspedalieri.vfx@gmail.com>
2020-12-06 15:16:33 +0100

    language covers and series added

--- js/script.js
+++ js/script.js
@@ -9,32 +9,77 @@
 //
 // Creare una select con i seguenti generi: pop, rock, metal e jazz.
 // In base a cosa scegliamo nella select vedremo solo i corrispondenti cd.
-const api_key= '010df9bf30e5f23360b2a681c219f1a0'
-const searchMovie = 'https://api.themoviedb.org/3/search/movie?'
+const api_key = '010df9bf30e5f23360b2a681c219f1a0';
 const movie_db = "https://api.themoviedb.org/3/search/movie";
+const series_db = "https://api.themoviedb.org/3/search/tv";
+
+
 
 var app = new Vue({
 
   el: "#app",
   data:{
     searched_movie:"",
-    movies:[], // array vuoto che riempio interrogando l api con axios
+    movies:[],
+    series:[],
+    mergedMedia:[]
+
   },
   methods:{
 
-    find:function () {
-    axios.get(movie_db,{
-      params:{
-          query:this.searched_movie,
-          api_key:api_key
-        }
-        }
-    ).then(answer => {
-      //popolo il mio array album con le response di
-      // ogni elemento disc che mi manda axios
+    findMovie:function () {
+    axios.get(movie_db,{params:{query:this.searched_movie,api_key:api_key}})
+    .then(answer => {
      this.movies = answer.data.results;
-     console.log("Movies:", this.movies);
+     this.movieResults();
+    })},
 
-   })}}
+    findTvseries:function () {
+    axios.get(series_db,{params:{query:this.searched_movie,api_key:api_key}})
+    .then(answer => {
+     this.series = answer.data.results;
+     this.tvSeriesResults();
 
+   })},
+
+   showSelected:function(){
+     this.findMovie();
+     this.findTvseries();
+     this.mergingResults();
+   },
+
+    //************ MOVIES SEARCH  ***************
+    movieResults: function(){
+
+     this.movies.forEach((film) => {
+       film.poster_path = "https://image.tmdb.org/t/p/w342" + film.poster_path;
+       film.vote_average = Math.ceil(film.vote_average / 2);
+       if (film.original_language != "") {
+         film.original_language = "img/flags/" + film.original_language + ".png";
+       }else{
+         film.original_language = "img/flags/unknown.png";
+       }
+     })
+   },
+
+   //************ TV SERIES SEARCH  ***************
+
+    tvSeriesResults: function(){
+
+     this.series.forEach((serie) => {
+       serie.poster_path = "https://image.tmdb.org/t/p/w342" + serie.poster_path;
+       serie.vote_average = Math.ceil(serie.vote_average / 2);
+       if (serie.original_language != null) {
+         serie.original_language = "img/flags/" + serie.original_language + ".png";
+       }else{
+         serie.original_language = "Language N/A";
+       }
+     })
+    },
+
+    mergingResults:function(){
+      this.mergedMedia = [...this.movies,...this.series];
+    }
+
+  }
 });
